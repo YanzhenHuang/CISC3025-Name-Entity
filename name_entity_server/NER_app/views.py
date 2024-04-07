da91045fa1c83974d70c177179def675f41ab7a9
Yanzhen Huang <huangyanzhen0108@163.com>
2024-04-07 16:07:40 +0800

    :pencil: Fix path issue

--- name_entity_server/NER_app/views.py
+++ name_entity_server/NER_app/views.py
@@ -3,8 +3,11 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 from django.contrib.staticfiles import finders
 
+import os
 from .NER import MEM
 from .NER import playground
+# from name_entity_server import settings
+
 
 def nerUI(request):
     return render(request, 'nerUI.html')
@@ -14,10 +17,11 @@ def resultView(request):
     input_query = request.POST.get("input-query", "<blank>")
 
     # Add your name entity processing here!!!
+    #TODO: Figure out a way to deal with this crap.
+    file_path = 'E:/Courses/CISC3025-Name-Entity/name_entity_server/name_entity_server/static/model.pkl'
+    names = playground.predict(input_query, MEM, file_path)
+    names = "(No names found)" if names == "" else names
+    output_query = names + " ---- from backend"
 
-    # model_pkl_path = finders.find('static/model.pkl')
-    # names = playground.predict(input_query, MEM)
-    # output_query = names + " ---- from backend"
-
-    output_query = input_query + " ---- from backend"
+    # output_query = input_query + " ---- from backend"
     return JsonResponse({"result": output_query})
