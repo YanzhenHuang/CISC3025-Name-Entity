3cf02fbf28750d82c6b7e4b1107c732ec7af27c9
Yanzhen Huang <huangyanzhen0108@163.com>
2024-04-07 19:07:56 +0800

    :pencil: Update Backend Result View

--- name_entity_server/NER_app/views.py
+++ name_entity_server/NER_app/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.http import JsonResponse
-from django.contrib.staticfiles import finders
 
 import os
 from .NER import MEM
 from .NER import playground
-# from name_entity_server import settings
 
 
 def nerUI(request):
@@ -20,6 +17,7 @@ def resultView(request):
     # Please never alter the path!!!!!
     model_pkl_path = os.path.abspath('name_entity_server/static/model.pkl').replace('\\', '/')
 
+    # Get modified name string
     names = playground.predict(input_query, MEM, model_pkl_path)
     output_query = names + " ---- from backend"
 
