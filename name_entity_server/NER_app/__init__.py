81a103779a00f652954229909c2bc514dff2c27d
YanzhenHuang <huangyanzhen0108@163.com>
2024-04-14 23:12:06 +0800

    :pencil: Update __init__.py to deal with MacOS stuff

--- name_entity_server/NER_app/__init__.py
+++ name_entity_server/NER_app/__init__.py
@@ -1,13 +1,27 @@
 import nltk
 import ssl
+import platform
 
-# # Disable ssl in case you use Mac computers.
-# try:
-#     _create_unverified_https_context = ssl._create_unverified_context
-# except AttributeError:
-#     pass
-# else:
-#     ssl._create_default_https_context = _create_unverified_https_context
-#
-# nltk.download('names')
-# nltk.download('stopwords')
+DO_NLTK_DOWNLOAD = True
+required_nltk_corpuses = ['names', 'stopwords', 'verbnet']
+
+
+def nltk_download():
+    if not DO_NLTK_DOWNLOAD:
+        return
+
+    if platform.system() == 'Darwin':
+        # I have to disable ssl in case you use Mac computers.
+        # Issue: https://github.com/python/cpython/issues/73666
+        try:
+            _create_unverified_https_context = ssl._create_unverified_context
+        except AttributeError:
+            pass
+        else:
+            ssl._create_default_https_context = _create_unverified_https_context
+
+    for corpus in required_nltk_corpuses:
+        nltk.download(corpus)
+
+
+nltk_download()
